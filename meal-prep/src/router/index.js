65fa30b6371cb548a3a198e55111d86623478cba
NiclasCravaack <57078667+NiclasCravaack@users.noreply.github.com>
2019-11-11 15:22:05 +0100

    bob

--- meal-prep/src/router/index.js
+++ meal-prep/src/router/index.js
@@ -2,28 +2,20 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Registration from "../views/Registration";
+import Wiki from "../views/Wiki";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "/Registration",
     name: "Registration",
     component: Registration
+  },
+  {
+    path:"/Wiki",
+    name:"Wiki",
+    component: Wiki
   }
 ];
 
