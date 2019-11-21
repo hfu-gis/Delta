498b0603f2ce9d57e6247e16022e74a31612bfe5
NickHaecker <56677688+NickHaecker@users.noreply.github.com>
2019-11-21 15:09:34 +0100

    impressum geschrieben

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,5 +1,15 @@
 <template>
-    <v-container>
+    <v-container class="Impressum">
+        <v-layout wrap text-center>
+            <v-flex xs12>
+                <h1>Impressum</h1>
+                <h2>Team Delta</h2>
+                    <p>Bots ist ein von Studenten entwickeltes Spiel </p>
+                    <p>
+                        vertreten durch Johann Schulenburg, Nick Häcker, Marius Kögel und Niclas Cravaack
+                    </p>
+                </v-flex>
+            </v-layout>
     </v-container>
 </template>
 
