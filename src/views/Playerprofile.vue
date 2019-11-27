f47f797fe7c0895cac61a04859815285ff28a18d
NickHaecker <56677688+NickHaecker@users.noreply.github.com>
2019-11-27 10:49:17 +0100

    profil überarbeitet

--- src/views/Playerprofile.vue
+++ src/views/Playerprofile.vue
@@ -1,73 +1,89 @@
 <template>
     <v-container class="body" xs12 sm6>
-        <section class="Profil">
-            <article class="header">
-                <header>
-                    <h1>Profile</h1>
-                </header>
-            </article>
-            <article class="settings">
-                <router-link to="Profileditor">
-                    <v-icon> mdi-account-settings</v-icon>
-                </router-link>
-            </article>
-        </section>
-        <section class="Profilinhalt">
-            <article class="profilbild">
-                <img src="../assets/122043.jpg" height="70" width="40"/>
-            </article>
-            <article class="profilname">
-                <h2>Lorem ipsum dolor </h2>
-            </article>
-        </section>
-        <v-container class="stats">
-            <section class="Statistiken">
-                <article class="statsmenü">
-                    <v-container class="routes">
-                        <router-link to="Profil" class="rout">Personal</router-link>
-                        <router-link to="Stats" class="rout" style="margin-left: 1%">Stats</router-link>
-                        <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
-                        <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
-                    </v-container>
-                </article>
-                <article class="profilstats">
-                    <header class="übprst">
-                        <h2>Personal Dates</h2>
+        <v-container>
+            <section class="Profil">
+                <article class="header">
+                    <header>
+                        <h1>Profile</h1>
                     </header>
-                    <v-container class="statstab">
-                        <v-simple-table class="simptabl1">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in desserts" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                        <v-simple-table class="simptabl2">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in dess" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                        <v-simple-table class="simptabl3">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in desser" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                    </v-container>
                 </article>
             </section>
+        </v-container>
+        <v-container>
+            <v-flex>
+                <v-container class="sidesettings">
+                    <aside class="settings">
+                        <v-list v-for="item in items" :key="item.title" :to="item.to" link>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </aside>
+                </v-container>
+                <v-container class="prf">
+                    <v-container class="Profilinhalt" >
+                        <section style="display: flex">
+                            <article class="profilbild">
+                                <img src="../assets/122043.jpg" height="70" width="40"/>
+                            </article>
+                            <article class="profilname">
+                                <h2>Lorem ipsum dolor </h2>
+                            </article>
+                        </section>
+                    </v-container>
+                    <v-container class="stats">
+                        <section class="Statistiken">
+                            <article class="statsmenü">
+                                <v-container class="routes">
+                                    <router-link to="Profil" class="rout">Personal</router-link>
+                                    <router-link to="Stats" class="rout" style="margin-left: 1%">Stats</router-link>
+                                    <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
+                                    <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
+                                </v-container>
+                            </article>
+                            <article class="profilstats">
+                                <header class="übprst">
+                                    <h2>Personal Dates</h2>
+                                </header>
+                                <v-container class="statstab">
+                                    <v-simple-table class="simptabl1">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in desserts" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                    <v-simple-table class="simptabl2">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in dess" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                    <v-simple-table class="simptabl3">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in desser" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                </v-container>
+                            </article>
+                        </section>
+                    </v-container>
+                </v-container>
+            </v-flex>
         </v-container>
     </v-container>
 </template>
@@ -144,6 +160,12 @@
                         info: 'Lorem',
                     },
                 ],
+                items: [
+                    {title: "General", to: "General"},
+                    {title: "Security and Login", to: "Security"},
+                    {title: "Privacy", to: "Privacy"},
+                    {title: "Language und Region", to: "Language"},
+                ],
             }
         },
     }
@@ -152,17 +174,17 @@
 
 <style scoped>
     .Profilinhalt {
-        display: flex;
-        width: 60%;
-        margin-left: 5%;
+        width: 48%;
+        margin-left: 26%;
+        margin-right: 26%;
         margin-top: 2%;
-        border: solid white;
         box-shadow: 1px 1px 1px black;
         background-color: #3D3D3D;
     }
 
     .Profil {
-        margin-left: 5%;
+        margin-left: 26%;
+        margin-right: 26%;
         margin-top: 0%;
     }
 
@@ -181,63 +203,58 @@
         font-size: 150%;
         margin-left: -1%;
     }
-
     .routes {
         margin-left: 3%;
     }
-
     .rout {
         box-sizing: border-box;
         padding: 1%;
         text-decoration: none;
     }
-
     .stats {
-        border: solid white;
+        width: 48%;
         box-shadow: 1px 1px 1px black;
-        width: 60%;
-        margin-left: 5%;
+        margin-left: 26%;
+        margin-right: 26%;
         margin-top: 2%;
         background-color: #3D3D3D;
     }
-
     .simptabl1 {
         width: 30%;
         margin-top: 2%;
     }
-
     .simptabl2 {
         width: 30%;
         margin-top: 2%;
         margin-left: 3%;
     }
-
     .simptabl3 {
         width: 30%;
         margin-top: 2%;
         margin-left: 3%;
     }
-
     .übprst {
         margin-top: 1%;
         margin-left: 6%;
     }
-
     .statstab {
         display: flex;
         margin-left: 2%;
     }
-
-    .body {
-        margin-left: -2%;
-    }
-
-    .settings {
-        margin-left: 70%;
-    }
-
     .header {
         font-size: 200%;
     }
-
+    .sidesettings{
+        float: left;
+        width: 15%;
+        margin-left: -2%;
+        margin-top: 10.5%;
+    }
+    .settings{
+        box-shadow: 1px 1px 1px black;
+        background-color: #3D3D3D;
+    }
+    .prf{
+        margin-top: -2%;
+    }
 </style>
--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,80 +1,99 @@
 <template>
     <v-container class="body" xs12 sm6>
-        <section class="Profil">
-            <article class="header">
-                <header>
-                    <h1>Profile</h1>
-                </header>
-            </article>
-            <article class="settings">
-                <router-link to="Profileditor">
-                    <v-icon> mdi-account-settings</v-icon>
-                </router-link>
-            </article>
-        </section>
-        <section class="Profilinhalt">
-            <article class="profilbild">
-                <img src="../assets/122043.jpg" height="70" width="40"/>
-            </article>
-            <article class="profilname">
-                <h2>Lorem ipsum dolor </h2>
-            </article>
-        </section>
-        <v-container class="stats">
-            <section class="Statistiken">
-                <article class="statsmenü">
-                    <v-container class="routes">
-                        <router-link to="Profil" class="rout">Personal</router-link>
-                        <router-link to="Stats" class="rout" style="margin-left: 1%">Stats</router-link>
-                        <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
-                        <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
-                    </v-container>
-                </article>
-                <article class="profilstats">
-                    <v-container class="statstab">
-                        <v-simple-table class="simptabl1">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in desserts" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                        <v-simple-table class="simptabl2">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in dess" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                        <v-simple-table class="simptabl3">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in desser" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                        <v-simple-table class="simptabl3">
-                            <template v-slot:default>
-                                <tbody>
-                                <tr v-for="item in desser" :key="item.name">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{item.info}}</td>
-                                </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
-                    </v-container>
+        <v-container>
+            <section class="Profil">
+                <article class="header">
+                    <header>
+                        <h1>Profile</h1>
+                    </header>
                 </article>
             </section>
+        </v-container>
+        <v-container>
+            <v-flex>
+                <v-container class="sidesettings">
+                    <aside class="settings">
+                        <v-list v-for="item in items" :key="item.title" :to="item.to" link>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </aside>
+                </v-container>
+                <v-container class="prf">
+                    <v-container class="Profilinhalt" >
+                        <section style="display: flex">
+                            <article class="profilbild">
+                                <img src="../assets/122043.jpg" height="70" width="40"/>
+                            </article>
+                            <article class="profilname">
+                                <h2>Lorem ipsum dolor </h2>
+                            </article>
+                        </section>
+                    </v-container>
+                    <v-container class="stats">
+                        <section class="Statistiken">
+                            <article class="statsmenü">
+                                <v-container class="routes">
+                                    <router-link to="Profil" class="rout">Personal</router-link>
+                                    <router-link to="Stats" class="rout" style="margin-left: 1%">Stats</router-link>
+                                    <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
+                                    <router-link to="" class="rout" style="margin-left: 1%">Lorem ipsum dolor</router-link>
+                                </v-container>
+                            </article>
+                            <article class="profilstats">
+                                <header class="übprst">
+                                    <h2>Personal Dates</h2>
+                                </header>
+                                <v-container class="statstab">
+                                    <v-simple-table class="simptabl1">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in desserts" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                    <v-simple-table class="simptabl2">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in dess" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                    <v-simple-table class="simptabl3">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in desser" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                    <v-simple-table class="simptabl4">
+                                        <template v-slot:default>
+                                            <tbody>
+                                            <tr v-for="item in desserer" :key="item.name">
+                                                <td>{{ item.name }}</td>
+                                                <td>{{item.info}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </template>
+                                    </v-simple-table>
+                                </v-container>
+                            </article>
+                        </section>
+                    </v-container>
+                </v-container>
+            </v-flex>
         </v-container>
     </v-container>
 </template>
@@ -102,6 +121,18 @@
                         info: 'Lorem ipsum dolor ',
                     },
                 ],
+                desserer: [
+                    {
+                        name: 'Lorem ipsum dolor ',
+                        info: 'Lorem ipsum dolor ',
+                    }
+                ],
+                items: [
+                    {title: "General", to: "General"},
+                    {title: "Security and Login", to: "Security"},
+                    {title: "Privacy", to: "Privacy"},
+                    {title: "Language und Region", to: "Language"},
+                ],
             }
         },
     }
@@ -109,17 +140,17 @@
 
 <style scoped>
     .Profilinhalt {
-        display: flex;
-        width: 60%;
-        margin-left: 5%;
+        width: 48%;
+        margin-left: 26%;
+        margin-right: 26%;
         margin-top: 2%;
-        border: solid white;
         box-shadow: 1px 1px 1px black;
         background-color: #3D3D3D;
     }
 
     .Profil {
-        margin-left: 5%;
+        margin-left: 26%;
+        margin-right: 26%;
         margin-top: 0%;
     }
 
@@ -138,63 +169,64 @@
         font-size: 150%;
         margin-left: -1%;
     }
-
     .routes {
         margin-left: 3%;
     }
-
     .rout {
         box-sizing: border-box;
         padding: 1%;
         text-decoration: none;
     }
-
     .stats {
-        border: solid white;
+        width: 48%;
         box-shadow: 1px 1px 1px black;
-        width: 60%;
-        margin-left: 5%;
+        margin-left: 26%;
+        margin-right: 26%;
         margin-top: 2%;
         background-color: #3D3D3D;
     }
-
     .simptabl1 {
         width: 30%;
         margin-top: 2%;
     }
-
     .simptabl2 {
         width: 30%;
         margin-top: 2%;
         margin-left: 3%;
     }
-
     .simptabl3 {
         width: 30%;
         margin-top: 2%;
         margin-left: 3%;
     }
-
+    .simptabl4{
+        width: 30%;
+        margin-top: 2%;
+        margin-left: 3%;
+    }
     .übprst {
         margin-top: 1%;
         margin-left: 6%;
     }
-
     .statstab {
         display: flex;
         margin-left: 2%;
     }
-
-    .body {
-        margin-left: -2%;
-    }
-
-    .settings {
-        margin-left: 70%;
-    }
-
     .header {
         font-size: 200%;
+    }
+    .sidesettings{
+        float: left;
+        width: 15%;
+        margin-left: -2%;
+        margin-top: 10.5%;
+    }
+    .settings{
+        box-shadow: 1px 1px 1px black;
+        background-color: #3D3D3D;
+    }
+    .prf{
+        margin-top: -2%;
     }
 
 </style>
