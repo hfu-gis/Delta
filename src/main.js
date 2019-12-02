cbbb9210ddb53bbabddb73a61cc80d1041a4a063
NickHaecker <56677688+NickHaecker@users.noreply.github.com>
2019-12-02 12:33:15 +0100

    Database

--- src/main.js
+++ src/main.js
@@ -14,11 +14,14 @@ new Vue({
   render: h => h(App),
   created(){
     firebase.initializeApp({
-      apiKey: 'AIzaSyAacup6WBgqvjFq9kFWmfdvZPt_TOZqARM',
-      authDomain: 'gis-deltaforce-bob.firebaseapp.com',
-      databaseURL: 'https://gis-deltaforce-bob.firebaseio.com',
-      projectId: 'gis-deltaforce-bob',
-      storageBucket: ''
+      apiKey: "AIzaSyAacup6WBgqvjFq9kFWmfdvZPt_TOZqARM",
+      authDomain: "gis-deltaforce-bob.firebaseapp.com",
+      databaseURL: "https://gis-deltaforce-bob.firebaseio.com",
+      projectId: "gis-deltaforce-bob",
+      storageBucket: '',
+      messagingSenderId: "385891058660",
+      appId: "1:385891058660:web:7669738eb06e4df8a63df1",
+      measurementId: "G-YNDBPRF37H"
     })
   }
 }).$mount("#app");
