e8fc63ce0f6389e4e9a532ea87d5448eb6107ad9
NickHaecker <56677688+NickHaecker@users.noreply.github.com>
2019-12-02 22:44:42 +0100

    Startnavigation

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-
+import colors from "vuetify/lib/util/colors";
 
 
 const vuetify = new Vuetify({
-
+    theme: {
+        dark:true,
+        themes: {
+            dark: {
+                primary: colors.grey,
+                secondary: colors.cyan,
+                accent: colors.purple,
+                error: colors.red,
+            },
+        },
+    },
 })
 Vue.use(Vuetify);
 
