9f7dc435a357dee1db609c60837e1b8907611ce0
NickHaecker <56677688+NickHaecker@users.noreply.github.com>
2020-01-13 18:13:03 +0100

    a

--- src/db.js
+++ src/db.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAacup6WBgqvjFq9kFWmfdvZPt_TOZqARM",
-  authDomain: "gis-deltaforce-bob.firebaseapp.com",
+  apiKey: "AIzaSyD9Rti5aPk9vzrj6V-CSwIlaJcnXn9kFz0",
+  authDomain: "deltashop-6a97f.firebaseapp.com",
   databaseURL: "https://deltashop-6a97f.firebaseio.com",
-  projectId: "gis-deltaforce-bob",
-  storageBucket: "gis-deltaforce-bob.appspot.com",
-  messagingSenderId: "385891058660",
-  appId: "1:385891058660:web:7669738eb06e4df8a63df1",
-  measurementId: "G-YNDBPRF37H"
+  projectId: "deltashop-6a97f",
+  storageBucket: "deltashop-6a97f.appspot.com",
+  messagingSenderId: "493723113811",
+  appId: "1:493723113811:web:1a5fdc607544257c6369a6",
+  measurementId: "G-PNKMG3ZL3W"
 };
 const db = firebase.initializeApp(firebaseConfig).firestore();
 
