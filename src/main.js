a81b9d7b77160e3fe32a89c4dab1567872316b3f
Koegel <marius.koegel@hs-furtwangen.de>
2020-01-19 19:48:22 +0100

    responsive test2

--- src/main.js
+++ src/main.js
@@ -1,29 +1,49 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { store } from "./store";
+import {store} from "./store";
 import vuetify from "./plugins/vuetify";
 import * as firebase from "firebase";
 import Alert from "./components/Alert";
+import VueMq from "vue-mq";
+
+Vue.use(VueMq, {
+    breakpoints: {
+        sm: 450,
+        md: 1250,
+        lg: Infinity,
+    }
+})
 
 Vue.config.productionTip = false;
 Vue.component("app-alert", Alert);
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App),
-  created() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyAacup6WBgqvjFq9kFWmfdvZPt_TOZqARM",
-      authDomain: "gis-deltaforce-bob.firebaseapp.com",
-      databaseURL: "https://gis-deltaforce-bob.firebaseio.com",
-      projectId: "gis-deltaforce-bob",
-      storageBucket: "gs://gis-deltaforce-bob.appspot.com",
-      messagingSenderId: "385891058660",
-      appId: "1:385891058660:web:7669738eb06e4df8a63df1",
-      measurementId: "G-YNDBPRF37H"
-    });
-  }
+    template: `
+    <grid-component :column="$mq | mq({
+      phone: 2,
+      tablet: 3,
+      laptop: 4
+    })">
+    </grid-component>
+  `,
+})
+
+new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App),
+    created() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAacup6WBgqvjFq9kFWmfdvZPt_TOZqARM",
+            authDomain: "gis-deltaforce-bob.firebaseapp.com",
+            databaseURL: "https://gis-deltaforce-bob.firebaseio.com",
+            projectId: "gis-deltaforce-bob",
+            storageBucket: "gs://gis-deltaforce-bob.appspot.com",
+            messagingSenderId: "385891058660",
+            appId: "1:385891058660:web:7669738eb06e4df8a63df1",
+            measurementId: "G-YNDBPRF37H"
+        });
+    }
 }).$mount("#app");
